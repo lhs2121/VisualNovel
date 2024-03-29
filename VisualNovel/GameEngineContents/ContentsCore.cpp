3b9af2f5e3e7324fe18d72973895887602c2c175
이한성 <mast1207@naver.com>
2024-03-30 00:03:03 +0900

    업데이트

--- VisualNovel/GameEngineContents/ContentsCore.cpp
+++ VisualNovel/GameEngineContents/ContentsCore.cpp
@@ -52,7 +52,7 @@ void ContentsCore::Start()
 	GameEngineCore::CreateLevel<TitleLevel>("TitleLevel");
 	GameEngineCore::CreateLevel<CityLevel>("CityLevel");
 	GameEngineCore::CreateLevel<IntroLevel>("IntroLevel");
-	GameEngineCore::ChangeLevel("CityLevel");
+	GameEngineCore::ChangeLevel("IntroLevel");
 }
 
 void ContentsCore::Update(float _Delta)
